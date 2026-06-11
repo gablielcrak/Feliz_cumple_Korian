8c37692b44f618244de6a4b49980ecdeed9b0329
Jose <josecarhuachinsulca@gmail.com>
2026-06-11 16:14:23 -0500

    Primer commit: Tarjeta de Korian

---
+++ script.js
@@ -0,0 +1,4 @@
+// Espera a que el botón esté listo y le asigna el evento de clic
+document.getElementById('btnCelebrar').addEventListener('click', function() {
+    alert("¡Que pases un día espectacular, Korian! 🎉🥳\nDisfruta mucho tu cumpleaños.");
+});
